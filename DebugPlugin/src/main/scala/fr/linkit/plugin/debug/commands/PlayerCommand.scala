3afeec5394709ee42feec8d4640facb805b8005f
Maxime <maximebatista18@gmail.com>
2021-08-26 13:44:42 +0200

    Fixing network initialization, added "innerInvocations" option for method behaviors that says if a method can have multiple inner invocations

--- DebugPlugin/src/main/scala/fr/linkit/plugin/debug/commands/PlayerCommand.scala
+++ DebugPlugin/src/main/scala/fr/linkit/plugin/debug/commands/PlayerCommand.scala
@@ -15,8 +15,8 @@ package fr.linkit.plugin.debug.commands
 import fr.linkit.api.connection.cache.SharedCacheManager
 import fr.linkit.api.connection.cache.obj.behavior.annotation.BasicInvocationRule
 import fr.linkit.engine.connection.cache.obj.DefaultSynchronizedObjectCenter
-import fr.linkit.engine.connection.cache.obj.behavior.SynchronizedObjectBehaviorBuilder.MethodControl
-import fr.linkit.engine.connection.cache.obj.behavior.{AnnotationBasedMemberBehaviorFactory, SynchronizedObjectBehaviorBuilder, SynchronizedObjectBehaviorStoreBuilder}
+import fr.linkit.engine.connection.cache.obj.behavior.ObjectBehaviorBuilder.MethodControl
+import fr.linkit.engine.connection.cache.obj.behavior.{AnnotationBasedMemberBehaviorFactory, ObjectBehaviorBuilder, ObjectBehaviorStoreBuilder}
 import fr.linkit.plugin.controller.cli.{CommandException, CommandExecutor, CommandUtils}
 
 import scala.collection.mutable.ListBuffer
@@ -26,8 +26,8 @@ class PlayerCommand(cacheHandler: SharedCacheManager, currentIdentifier: String)
     /*println("Press enter to continue...")
     new Scanner(System.in).nextLine()*/
 
-    private val tree = new SynchronizedObjectBehaviorStoreBuilder(AnnotationBasedMemberBehaviorFactory) {
-        behaviors += new SynchronizedObjectBehaviorBuilder[ListBuffer[Player]]() {
+    private val tree = new ObjectBehaviorStoreBuilder(AnnotationBasedMemberBehaviorFactory) {
+        behaviors += new ObjectBehaviorBuilder[ListBuffer[Player]]() {
             annotateAllMethods("+=") and "addOne" by new MethodControl(BasicInvocationRule.BROADCAST)
         }
     }.build
